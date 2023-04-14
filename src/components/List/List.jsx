6103064cbbc2b6adf9709145583c7d403e1e1bea
Marvin <marvinmaduike@gmail.com>
2023-04-14 12:27:04 +0100

    tesss

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -35,7 +35,7 @@ const List = ({title,text1,text2}) => {
             { toggleMenu && (
                 <div className='app__list_dp'>
 
-                <div className='svg__div__text' >
+                <div className='svg__div_text' >
                     <p>
                         {text1}
                    </p>
